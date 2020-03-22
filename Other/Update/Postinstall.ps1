79ce865831b71bcf0b0fd3da2a47a3421ce99ed7
Urs Roesch <github@bun.ch>
2020-03-22 14:14:46 +0100

    Beta1: Add update and postinstall scripts
    Summary:
      * Automatic download and installation
        via `Update.ps1` and `Postinstall.ps1`

---
+++ Other/Update/Postinstall.ps1
@@ -0,0 +1,20 @@
+# Run the post-install script
+
+$_PostInstall = "$AppDir\Git\post-install.bat" 
+
+If (Test-Path $_PostInstall) {
+  Switch (Is-Unix) {
+    $True   { $_Prefix = 'wine'; break }
+    default { $_Prefix = '';     break }
+  }
+  Try {
+    Debug info "Run post install command $_Prefix $_PostInstall"
+    # Will always throw an error as the post-install script
+    # is removing itself at the end.
+    Invoke-Expression "$_Prefix $_PostInstall" 2>&1 | Out-Null
+  }
+  Catch {
+    Debug fatal "Failed to run command $_PostInstall"
+    Exit 127
+  }
+}
