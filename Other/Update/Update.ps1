012f3d20f2c38dbe2387036211af6d429fa3a21f
Urs Roesch <github@bun.ch>
2020-09-29 21:14:38 +0000

    Debug

--- Other/Update/Update.ps1
+++ Other/Update/Update.ps1
@@ -12,7 +12,8 @@ Using module ".\PortableAppsCommon.psm1"
 # -----------------------------------------------------------------------------
 # Globals
 # -----------------------------------------------------------------------------
-$Version = "0.0.24-alpha"
+Set-PSDebug -Trace 1
+$Version = "0.0.25-alpha"
 $Debug   = $True
 
 # -----------------------------------------------------------------------------
@@ -269,11 +270,6 @@ Function Invoke-Helper() {
       $Arguments = ConvertTo-WindowsPath $AppPath
     }
   }
-
-  #If ($Sleep) {
-  #  Debug info "Waiting for filsystem cache to catch up"
-  #  Start-Sleep $Sleep
-  #}
 
   Debug info "Run PA $Command $Arguments"
   Start-Process $Command -ArgumentList $Arguments -NoNewWindow -Wait
